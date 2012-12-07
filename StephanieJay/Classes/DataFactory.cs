80dfdb185aee701f2e6b512b35caf62fba096e63
Alex Hunter <al.hunter@hotmail.co.uk>
2012-12-07 21:17:42 +0000

    More Crazy crazy stuff

--- StephanieJay/Classes/DataFactory.cs
+++ StephanieJay/Classes/DataFactory.cs
@@ -1,37 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Xml.Linq;
-using StephanieJay.Classes;
-using StephanieJay.Models;
-
-namespace StephanieJay.Classes
+﻿namespace StephanieJay.Classes
 {
-    public static class DataFactory
+    public class DataFactory
     {
-        public static NewsItemModel GetNews()
+        public DataFactory()
         {
-            var news = new NewsItemModel();
-            var newsItems = new List<NewsItem>(); 
-
-            XDocument rss = XDocument.Load(HttpRuntime.AppDomainAppPath + "/XML/News.xml");
-            foreach (XElement item in rss.Descendants("item"))
-            {
-                var newsItem = new NewsItem 
-                {
-                    Title = item.Element("title").Value,
-                    Description = item.Element("description").Value,
-                    PubDate = item.Element("pubDate").Value,
-                    Link = item.Element("link").Value
-                };
-
-                newsItems.Add(newsItem);
-            }
-
-            news.NewsItems = newsItems;
-            return news;
+            this.News = new NewsData();
         }
+
+        public NewsData News { get; set; }
     }
 }
--- StephanieJay/Controllers/AdminController.cs
+++ StephanieJay/Controllers/AdminController.cs
@@ -9,17 +9,88 @@ using StephanieJay.Models;
 
 namespace StephanieJay.Controllers
 {
-    public class AdminController : Controller
+    public class NewsController : Controller
     {
+        private DataFactory _dataFactory = new DataFactory();
+
+        //GET: /News
         public ActionResult Index()
+        {
+            var news = _dataFactory.News.Get();
+            return View(news);
+        }
+
+        //GET: /News/Create
+        public ActionResult Create()
         {
             return View();
         }
 
-        public ActionResult EditNews()
+        //POST: /News/Create
+        [HttpPost]
+        public ActionResult Create(News news)
         {
-            NewsItemModel news = DataFactory.GetNews();
+            if (ModelState.IsValid)
+            {
+                _dataFactory.News.Add(news);
+                _dataFactory.News.Save();
+                return RedirectToAction("Index");
+            }
             return View(news);
+        }
+
+
+        //
+        // GET: /News/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            News news = _dataFactory.News.Get(id);
+            if (news == null)
+            {
+                return HttpNotFound();
+            }
+            return View(news);
+        }
+
+        //
+        // POST: /News/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(News news)
+        {
+            if (ModelState.IsValid)
+            {
+                //_dataFactory.News.Entry(news).State = EntityState.Modified;
+                _dataFactory.News.Save();
+                return RedirectToAction("Index");
+            }
+            return View(news);
+        }
+
+        //
+        // GET: /News/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            News news = _dataFactory.News.Get(id);
+            if (news == null)
+            {
+                return HttpNotFound();
+            }
+            return View(news);
+        }
+
+        //
+        // POST: /News/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            News news = _dataFactory.News.Get(id);
+            _dataFactory.News.Remove(news);
+            _dataFactory.News.Save();
+            return RedirectToAction("Index");
         }
     }
 }
