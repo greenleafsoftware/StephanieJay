1f940b0bea3797a09f92d36da418616e7411e992
Alex Hunter <al.hunter@hotmail.co.uk>
2012-12-12 23:13:28 +0000

    shizzle ma nizzle

--- StephanieJay/Classes/Channel.cs
+++ StephanieJay/Classes/Channel.cs
@@ -1,42 +1,16 @@
-﻿using System;
+﻿using System.Xml.Serialization;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Xml.Serialization;
 
 namespace RSS
 {
     [XmlRootAttribute(ElementName = "channel")]
     public class Channel : Element
     {
-        public string copyright
-        {
-            get { return _copyright; }
-            set { _copyright = value; }
-        }
-
-        public string language
-        {
-            get { return _language; }
-            set { _language = value; }
-        }
-
-        public Image image
-        {
-            get { return _image; }
-            set { _image = value; }
-        }
+        public string copyright { get; set; }
+        public string language { get; set; }
+        public Image image { get; set; }
 
         [XmlElement(ElementName = "item", Type = typeof(Item))]
-        public List<Item> items
-        {
-            get { return _item; }
-            set { _item = value; }
-        }
-
-        private string _copyright;
-        private string _language;
-        private Image _image;
-        private List<Item> _item;
+        public List<Item> items { get; set; }
     }
 }
--- StephanieJay/Classes/Image.cs
+++ StephanieJay/Classes/Image.cs
@@ -1,15 +1,10 @@
 ﻿using System.Xml.Serialization;
+
 namespace RSS
 {
     [XmlRootAttribute(ElementName = "image")]
     public class Image : Element
     {
-        public string url
-        {
-            get { return _url; }
-            set { _url = value; }
-        }
-
-        private string _url;
+        public string url { get; set; }
     }
 }
