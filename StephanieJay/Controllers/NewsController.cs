b63ebc28295b0272639c418bdce1f0b2cd1b5a55
Alex Hunter <al.hunter@hotmail.co.uk>
2012-12-14 00:23:33 +0000

    Fixing merge... bah!

--- StephanieJay/Controllers/NewsController.cs
+++ StephanieJay/Controllers/NewsController.cs
@@ -7,6 +7,7 @@ using System.Xml.Linq;
 using StephanieJay.Models;
 using System.Net;
 using RSS;
+using System.Web.Configuration;
 
 namespace StephanieJay.Controllers
 {
@@ -16,14 +17,11 @@ namespace StephanieJay.Controllers
 
         public NewsController()
         {
-<<<<<<< HEAD
-            /*WebRequest request = WebRequest.CreateDefault(new Uri("http://www.greenleafsoftware.co.uk/XML/News.xml"));
-=======
-            WebRequest request = WebRequest.CreateDefault(new Uri(System.Web.Configuration.WebConfigurationManager.AppSettings["urlRSS"]));
->>>>>>> 6fe9a243b7b865cf78e8732e106082aa84470119
+            /*WebRequest request = WebRequest.CreateDefault(new Uri(WebConfigurationManager.AppSettings["urlRSS"]));
             WebResponse response = request.GetResponse();
             _newsRss = Rss.Load(response.GetResponseStream());*/
-            _newsRss = Rss.Load("C:\\Users\\Al\\Desktop\\test.xml");
+
+            _newsRss = Rss.Load(Server.MapPath(WebConfigurationManager.AppSettings["LocalRSS"]));
         }
 
         //GET: /News
@@ -46,12 +44,7 @@ namespace StephanieJay.Controllers
             if (ModelState.IsValid)
             {
                 _newsRss.channel.items.Add(news);
-<<<<<<< HEAD
-                //_newsRss.Save(Server.MapPath("..//rss.xml"));
-                _newsRss.Save("C:\\Users\\Al\\Desktop\\test.xml");
-=======
-                _newsRss.Save(Server.MapPath(System.Web.Configuration.WebConfigurationManager.AppSettings["LocalRSS"]));
->>>>>>> 6fe9a243b7b865cf78e8732e106082aa84470119
+                _newsRss.Save(Server.MapPath(WebConfigurationManager.AppSettings["LocalRSS"]));
                 return RedirectToAction("Index");
             }
             return View(news);
@@ -74,15 +67,9 @@ namespace StephanieJay.Controllers
         {
             if (ModelState.IsValid)
             {
-<<<<<<< HEAD
                 _newsRss.channel.items.RemoveAll(x => x.guid == news.guid);
                 _newsRss.channel.items.Add(news);
-                _newsRss.Save("C:\\Users\\Al\\Desktop\\test.xml");
-=======
-                //_dataFactory.News.Entry(news).State = EntityState.Modified;
-                //_newsRss.Save(Server.MapPath("..//rss.xml"));
-                _newsRss.Save(Server.MapPath(System.Web.Configuration.WebConfigurationManager.AppSettings["LocalRSS"]));
->>>>>>> 6fe9a243b7b865cf78e8732e106082aa84470119
+                _newsRss.Save(Server.MapPath(WebConfigurationManager.AppSettings["LocalRSS"]));
                 return RedirectToAction("Index");
             }
             return View(news);
@@ -91,13 +78,8 @@ namespace StephanieJay.Controllers
         // GET: /News/Delete/5
         public ActionResult Delete(string id)
         {
-<<<<<<< HEAD
             _newsRss.channel.items.RemoveAll(x => x.guid == id);
-            _newsRss.Save("C:\\Users\\Al\\Desktop\\test.xml");
-=======
-            _newsRss.channel.items.RemoveAt(id);
-            _newsRss.Save(Server.MapPath(System.Web.Configuration.WebConfigurationManager.AppSettings["LocalRSS"]));
->>>>>>> 6fe9a243b7b865cf78e8732e106082aa84470119
+            _newsRss.Save(Server.MapPath(WebConfigurationManager.AppSettings["LocalRSS"]));
             return RedirectToAction("Index");
         }
     }
