805878f5fc409a9f4f184d4149574e644ba9714c
Alex Hunter <al.hunter@hotmail.co.uk>
2013-01-13 23:22:47 +0000

    Add

---
+++ StephanieJay/Controllers/AdminController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;
+using StephanieJay.Models;
+using System.Net;
+using RSS;
+using System.Web.Configuration;
+
+namespace StephanieJay.Controllers
+{
+    [Authorize(Users="Stephanie")]
+    public class NewsController : Controller
+    {
+        private Rss _newsRss;
+
+        public NewsController()
+        {
+            _newsRss = Rss.Load(System.Web.HttpContext.Current.Server.MapPath(WebConfigurationManager.AppSettings["News"]));
+        }
+
+        //GET: /News
+        public ActionResult Index()
+        {
+            var news = _newsRss.channel.items;
+            return View(news);
+        }
+
+        //GET: /News/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //POST: /News/Create
+        [HttpPost]
+        public ActionResult Create(Item news)
+        {
+            if (ModelState.IsValid)
+            {
+                _newsRss.channel.items.Add(news);
+                _newsRss.Save(System.Web.HttpContext.Current.Server.MapPath(WebConfigurationManager.AppSettings["News"]));
+                return RedirectToAction("Index");
+            }
+            return View(news);
+        }
+
+        // GET: /News/Edit/5
+        public ActionResult Edit(string id)
+        {
+            Item news = _newsRss.channel.items.FirstOrDefault(x => x.guid == id);
+            if (news == null)
+            {
+                return HttpNotFound();
+            }
+            return View(news);
+        }
+
+        // POST: /News/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Item news)
+        {
+            if (ModelState.IsValid)
+            {
+                _newsRss.channel.items.RemoveAll(x => x.guid == news.guid);
+                _newsRss.channel.items.Add(news);
+                _newsRss.Save(System.Web.HttpContext.Current.Server.MapPath(WebConfigurationManager.AppSettings["News"]));
+                return RedirectToAction("Index");
+            }
+            return View(news);
+        }
+
+        // GET: /News/Delete/5
+        public ActionResult Delete(string id)
+        {
+            _newsRss.channel.items.RemoveAll(x => x.guid == id);
+            _newsRss.Save(System.Web.HttpContext.Current.Server.MapPath(WebConfigurationManager.AppSettings["News"]));
+            return RedirectToAction("Index");
+        }
+    }
+}
--- StephanieJay/Global.asax.cs
+++ StephanieJay/Global.asax.cs
@@ -22,6 +22,12 @@ namespace StephanieJay
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                "Admin",
+                "Admin/{controller}/{action}/{id}",
+                new { controller = "Admin", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
                 new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
