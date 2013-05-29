512bb40a65b98f94dba3ece211d187acd9d98a16
Alex Hunter <al.hunter@hotmail.co.uk>
2013-05-30 00:02:37 +0100

    Gigs again

--- StephanieJay/Areas/Admin/Controllers/GigsController.cs
+++ StephanieJay/Areas/Admin/Controllers/GigsController.cs
@@ -43,62 +43,9 @@ namespace StephanieJay.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
-<<<<<<< HEAD
-                _gigs.gigs.Add(gig);
-||||||| merged common ancestors
-                //Check if group exists for gig
-                Predicate<Group> search = gr => gr.date.Month == gig.date.Month && gr.date.Year == gig.date.Year;
-
-                if (_gigs.groups.Exists(search))
-                {
-                    //Add the gig to the existing group.
-                    _gigs.groups.Find(search).gigs.Add(gig);
-                }
-                else
-                {
-                    //Create a new group and add the gig.
-                    var dateInfo = new DateTimeFormatInfo();
-                    var group = new Group
-                    {
-                        date = new DateTime(gig.date.Year, gig.date.Month, 1),
-                        name = dateInfo.GetMonthName(gig.date.Month) + " " + gig.date.Year,
-                        gigs = new List<Gig> { gig }
-                    };
-                    _gigs.groups.Add(group);
-                }
-
-=======
-                ////Give it a new guid
-                //if (gig.guid == null)
-                //{
-                //    gig.guid = Guid.NewGuid().ToString();
-                //}
-
-                ////Check if group exists for gig
-                //Predicate<Group> search = gr => gr.date.Month == gig.date.Month && gr.date.Year == gig.date.Year;
-
-                //if (_gigs.groups.Exists(search))
-                //{
-                //    //Add the gig to the existing group.
-                //    _gigs.groups.Find(search).gigs.Add(gig);
-                //}
-                //else
-                //{
-                //    //Create a new group and add the gig.
-                //    var dateInfo = new DateTimeFormatInfo();
-                //    var group = new Group
-                //    {
-                //        date = new DateTime(gig.date.Year, gig.date.Month, 1),
-                //        name = dateInfo.GetMonthName(gig.date.Month) + " " + gig.date.Year,
-                //        gigs = new List<Gig> { gig }
-                //    };
-                //    _gigs.groups.Add(group);
-                //}
-
                 gig.guid = Guid.NewGuid().ToString();
                 _gigs.gigs.Add(gig);
-                _gigs.gigs.Sort((a, b) => a.date.CompareTo(b.date));
->>>>>>> c7fd00a9faa6d0653e67aff3003e4611da0052f2
+                _gigs.gigs.OrderBy(g => g.date);
                 Xml<Gigs>.Save(_xmlPath, _gigs);
                 return RedirectToAction("Index");
             }
@@ -108,14 +55,7 @@ namespace StephanieJay.Areas.Admin.Controllers
         // GET: /Admin/Gigs/Edit/5
         public ActionResult Edit(string id)
         {
-<<<<<<< HEAD
-            Gig gig = _gigs.gigs.SingleOrDefault(g => g.guid == id);
-||||||| merged common ancestors
-            Gig gig = _gigs.groups.SelectMany(gp => gp.gigs).SingleOrDefault(g => g.guid == id);
-=======
-            //Gig gig = _gigs.groups.SelectMany(gp => gp.gigs).SingleOrDefault(g => g.guid == id);
             Gig gig = _gigs.gigs.FirstOrDefault(x => x.guid == id);
->>>>>>> c7fd00a9faa6d0653e67aff3003e4611da0052f2
             if (gig == null)
             {
                 return HttpNotFound();
@@ -129,21 +69,10 @@ namespace StephanieJay.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
-<<<<<<< HEAD
-                _gigs.gigs.Remove(gig);
+                _gigs.gigs.RemoveAll(x => x.guid == gig.guid);
                 _gigs.gigs.Add(gig);
-                //Xml<Rss>.Save(_xmlPath, _gigs);
-||||||| merged common ancestors
-                _gigs.xxxx.RemoveAll(x => x.guid == model.guid);
-                _gigs.xxxx.gigs.Add(model);
-                Xml<Rss>.Save(_xmlPath, _gigs);
-=======
-                //_gigs.groups.SelectMany(g => g.gigs).ToList().RemoveAll(g => g.guid == gig.guid);
-                _gigs.gigs.RemoveAll(x => x.guid == model.guid);
-                _gigs.gigs.Add(model);
-                _gigs.gigs.Sort((a, b) => a.date.CompareTo(b.date));
+                _gigs.gigs.OrderBy(g => g.date);
                 Xml<Gigs>.Save(_xmlPath, _gigs);
->>>>>>> c7fd00a9faa6d0653e67aff3003e4611da0052f2
                 return RedirectToAction("Index");
             }
             return RedirectToAction("Index");
@@ -152,9 +81,6 @@ namespace StephanieJay.Areas.Admin.Controllers
         // GET: /News/Delete/5
         public ActionResult Delete(string id)
         {
-            /*_newsRss.channel.items.RemoveAll(x => x.guid == id);
-            Xml<Rss>.Save(_xmlPath, _newsRss);*/
-            //_gigs.groups.SelectMany(g => g.gigs).ToList().RemoveAll(g => g.guid == id);
             _gigs.gigs.RemoveAll(x => x.guid == id);
             Xml<Gigs>.Save(_xmlPath, _gigs);
             return RedirectToAction("Index");
